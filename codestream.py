3c8fbffa90be6203f5800d1f8d14958f2bf03b5c
Anthony <86032299+Anthony9933@users.noreply.github.com>
2024-05-16 15:59:28 -0300

    Update codestream.py

--- codestream.py
+++ codestream.py
@@ -37,78 +37,9 @@ def show_overview():
 
 def show_filters_data():
     st.header("Filtros e Dados")
-    df = pd.read_csv('', encoding='latin-1', delimiter=',')
+    df = pd.read_csv('dados.csv', encoding='latin-1', delimiter=';')
     st.header('Gráficos')
     st.dataframe(df)
-    UF = st.sidebar.selectbox('Selecione o UF', options=df['uf'].unique())
-
-    # Filtrando o dataframe para apenas linhas do UF escolhido
-    df_uf = df[df['uf'] == UF]
-
-    # Contando o número de acidentes por município
-    contagem_acidentes_por_municipio = df_uf['municipio'].value_counts().reset_index()
-
-    # Renomeando as colunas para melhor entendimento
-    contagem_acidentes_por_municipio.columns = ['Município', 'Quantidade de Acidentes']
-
-    # Criando o gráfico
-    fig = px.bar(contagem_acidentes_por_municipio, x='Município', y='Quantidade de Acidentes',
-                 title='Quantidade de Acidentes por Município')
-    st.plotly_chart(fig)
-
-
-    soma_mortos_por_municipio = df_uf.groupby('municipio')['mortos'].sum().reset_index()
-
-    # Renomeando as colunas para melhor entendimento
-    soma_mortos_por_municipio.columns = ['Município', 'Quantidade de Mortos']
-
-    # Criando o gráfico
-    fig2 = px.line(soma_mortos_por_municipio, x='Município', y='Quantidade de Mortos',
-                 title='Quantidade de Mortos por Município')
-
-    st.plotly_chart(fig2)
-
-#def show_graphs():
-    st.header("Gráficos de Acidentes e Casualidades ao Longo do Tempo")
-    df = pd.read_csv('datatran2023.csv', encoding='latin-1', delimiter=';')
-    
-
-    # Convertendo a coluna 'data_inversa' para o tipo datetime
-    df['data_inversa'] = pd.to_datetime(df['data_inversa'])
-
-    # Agrupando os dados pela data e contando o número de acidentes
-    accidents_count = df.groupby('data_inversa').size().reset_index(name='Número de Acidentes')
-
-    # Criando o gráfico de linha para o número de acidentes ao longo do tempo
-    fig3 = px.line(accidents_count, x='data_inversa', y='Número de Acidentes',
-                  title='Número de Acidentes ao Longo do Tempo')
-    st.plotly_chart(fig3)
-
-    # Agrupando os dados pela data e somando o número de mortos, feridos leves, feridos graves e ilesos
-    casualties = df.groupby('data_inversa').agg({'mortos': 'sum', 'feridos_leves': 'sum', 'feridos_graves': 'sum', 'ilesos': 'sum'}).reset_index()
-
-    # Criando o gráfico de linha para o número de mortos, feridos leves, feridos graves e ilesos ao longo do tempo
-    fig4 = px.line(casualties, x='data_inversa', y='mortos', title='Número de Mortos ao Longo do Tempo')
-    fig4.add_scatter(x=casualties['data_inversa'], y=casualties['feridos_leves'], mode='lines', name='Feridos Leves')
-    fig4.add_scatter(x=casualties['data_inversa'], y=casualties['feridos_graves'], mode='lines', name='Feridos Graves')
-    fig4.add_scatter(x=casualties['data_inversa'], y=casualties['ilesos'], mode='lines', name='Ilesos')
-    st.plotly_chart(fig4)
-
-#def show_accidents_by_hour():
-    st.header("Número de Acidentes por Hora do Dia")
-    df = pd.read_csv('datatran2023.csv', encoding='latin-1', delimiter=';')
-
-    # Convertendo a coluna 'horario' para o tipo datetime e extraindo a hora
-    df['hora'] = pd.to_datetime(df['horario']).dt.hour
-
-    # Agrupando os dados pela hora e contando o número de acidentes
-    accidents_count = df.groupby('hora').size().reset_index(name='Número de Acidentes')
-
-    # Criando o gráfico de linha
-    fig5 = px.bar(accidents_count, x='hora', y='Número de Acidentes',
-                  title='Número de Acidentes por Hora do Dia')
-    st.plotly_chart(fig5)
-
 
 
 # Página de Visão Geral
