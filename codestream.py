53be8366ea741bdd8d15b8e21c685310be048563
EnricoCassimiro <155919496+EnricoCassimiro@users.noreply.github.com>
2024-05-20 20:57:33 -0300

    Update codestream.py

--- codestream.py
+++ codestream.py
@@ -39,15 +39,7 @@ def show_overview():
 
 def show_filters_data():
     st.header("Filtros e Dados")
-    df_BLAZER = pd.read_csv('BLAZER - Página1.csv', encoding='latin-1', delimiter=',')
-    df_BERMUDA = pd.read_csv('BERMUDA - Página1.csv', encoding='latin-1', delimiter=',')
-    df_BLUSA = pd.read_csv('BLUSA - Página1.csv', encoding='latin-1', delimiter=',')
-    df_CALÇA = pd.read_csv('CALÇA - Página1.csv', encoding='latin-1', delimiter=',')
-    df_CAMISA = pd.read_csv('CAMISA - Página1.csv', encoding='latin-1', delimiter=',')
-    df_CROPPED = pd.read_csv('CROPPED - Página1.csv', encoding='latin-1', delimiter=',')
-    df_SAIA = pd.read_csv('SAIA - Página1.csv', encoding='latin-1', delimiter=',') 
-    df_SHORT = pd.read_csv('SHORT - Página1.csv', encoding='latin-1', delimiter=',') 
-    df_TSHIRT = pd.read_csv('T-SHIRT - Página1.csv', encoding='latin-1', delimiter=',') 
+    df = pd.read_csv('BLAZER - Página1.csv', encoding='latin-1', delimiter=',')
     st.header('Gráficos')
     st.dataframe(df)
     # Criar uma nova coluna 'Categoria'
