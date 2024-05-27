d750a608795efba970e8e2f14924be9cd82e20b6
EnricoCassimiro <155919496+EnricoCassimiro@users.noreply.github.com>
2024-05-27 17:19:00 -0300

    Update codestream.py

--- codestream.py
+++ codestream.py
@@ -37,7 +37,7 @@ def show_overview():
 
 def show_filters_data():
     st.header("Filtros e Dados")
-   
+    st.warning("ESTAMOS EM FASE DE TESTES! ASS: DESENVOLVEDORES")
     # Lista de arquivos CSV
     arquivos = ['BLAZER - Página1.csv', 'BERMUDA - Página1.csv', 'BLUSA - Página1.csv', 'CALÇA - Página1.csv', 'CAMISA - Página1.csv', 'CROPPED - Página1.csv', 'SAIA - Página1.csv', 'SHORT - Página1.csv', 'T-SHIRT - Página1.csv']
     
@@ -49,7 +49,7 @@ def show_filters_data():
     st.dataframe(df)
 
 ###FASE DE TESTE PARA GRAFICOS###
-    st.warning("ESTAMOS EM FASE DE TESTES! ASS: DESENVOLVEDORES")
+    
    # Lista de arquivos CSV
     arquivos = ['dados (8).csv']
     
