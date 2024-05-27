8230744c2973db5aa25f971931cca80d9e2e1cbb
EnricoCassimiro <155919496+EnricoCassimiro@users.noreply.github.com>
2024-05-27 16:22:51 -0300

    Update codestream.py

--- codestream.py
+++ codestream.py
@@ -37,47 +37,10 @@ def show_overview():
 
 def show_filters_data():
     st.header("Filtros e Dados")
-    df_blazer = pd.read_csv('BLAZER - Página1.csv', encoding='latin-1', delimiter=',')
+    df = pd.read_csv('BLAZER - Página1.csv', encoding='latin-1', delimiter=',')
     st.header('Gráficos')
     st.dataframe(df)
     
-    # Criar uma nova coluna 'Categoria'
-    #df['Categoria'] = df['DESCRIÇÃO']
-    
-    # Lista de tipos de t-shirts
-    #tipos_tshirts = ['t-shirt tipo1', 't-shirt tipo2', 't-shirt tipo3']  # substitua pelos nomes reais dos tipos de t-shirts
-    
-    # Agrupar todos os tipos de t-shirts em uma única categoria
-    #df.loc[df['Categoria'].isin(tipos_tshirts), 'Categoria'] = 'T-Shirt'
-    
-    # Agora você pode usar a coluna 'Categoria' para suas análises
-
-
-    # Calcular o lucro provável
-    # Criar um novo DataFrame contendo apenas os itens que contêm "t-shirt"
-    df_tshirt = df[df['DESCRICAO'].str.contains('T-SHIRT', case=False, na=False)]
-    df_camisa = df[df['DESCRICAO'].str.contains('CAMISA', case=False, na=False)]
-    df_cropped = df[df['DESCRICAO'].str.contains('CROPPED', case=False, na=False)]
-    df_bermuda = df[df['DESCRICAO'].str.contains('BERMUDA', case=False, na=False)]
-    df_blusa = df[df['DESCRICAO'].str.contains('BLUSA', case=False, na=False)]
-    df_short = df[df['DESCRICAO'].str.contains('SHORT', case=False, na=False)]
-    df_calça = df[df['DESCRICAO'].str.contains('CALCA', case=False, na=False)]
-    df_saia = df[df['DESCRICAO'].str.contains('SAIA', case=False, na=False)]
-    df_blazer = df[df['DESCRICAO'].str.contains('BLAZER', case=False, na=False)]
-    df_colete = df[df['DESCRICAO'].str.contains('COLETE', case=False, na=False)]
-    df_jaqueta = df[df['DESCRICAO'].str.contains('JAQUETA', case=False, na=False)]
-    
-    fig, axs = plt.subplots(2, 2, figsize=(15, 10))
-    axs[0, 1].hist(df_tshirts['DESCRICAO'], bins=20, color='blue', alpha=0.7)
-    axs[0, 1].set_title('T-Shirts')
-    axs[0, 1].hist(df_camisa['DESCRICAO'], bins=20, color='green', alpha=0.7)
-    axs[0, 1].set_title('Camisas')
-    axs[1, 0].hist(df_cropped['DESCRICAO'], bins=20, color='red', alpha=0.7)
-    axs[1, 0].set_title('Cropped')
-    axs[1, 1].hist(df_bermuda['DESCRICAO'], bins=20, color='purple', alpha=0.7)
-    axs[1, 1].set_title('Bermudas')
-    plt.tight_layout()
-    st.pyplot(fig)
     
 # Página de Visão Geral
 if page == "Visão Geral":
